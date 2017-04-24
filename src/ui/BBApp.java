0a3c3b5221d297c49c3b0a498b7231e0ba1258eb
Jeffrey Miller <Millerjl07@mounties.mansfield.edu>
2017-04-24 08:39:42 -0400

    Forgotten changes

--- src/ui/BBApp.java
+++ src/ui/BBApp.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import linkedlist2.Friend;
 import linkedlist2.Link;
@@ -17,15 +18,23 @@ public class BBApp {
 
     public BBApp() {
         this.list = new LinkedList();
+        list.push(new Friend("Bob", 0));
+        list.push(new Friend("Kim", 1));
+        list.push(new Friend("Pam", 4));
+        list.push(new Friend("Tony", 2));
+        list.push(new Friend("Robert", 7));
+        list.push(new Friend("Sim", 4));
+        list.push(new Friend("Carl", 3));
+        list.push(new Friend("Jasper", 0));
         start();
     }
 
-    public void start() {
+    private void start() {
         Scanner sc = new Scanner(System.in);
-        String choice = "";
+        String choice;
         while (true) {
             menu();
-            String regex = "^quit|add|remove|list$";
+            String regex = "^quit|add|remove|list|name|reverse|rating$";
             choice = Validator.getLine(sc, "Enter your choice: ", regex);
             if (choice.equalsIgnoreCase("quit")) {
                 break;
@@ -33,8 +42,34 @@ public class BBApp {
                 String name = Validator.getLine(sc, "Enter friend's name: ");
                 int rating = Validator.getInt(sc, "Enter rating: ");
                 list.push(new Friend(name, rating));
-            } else if (choice.equalsIgnoreCase("list")) {
+            } 
+            else if(choice.equalsIgnoreCase("remove")){
+                int index = Validator.getInt(sc, "Enter index to remove: ");
+                list.delete(index);
+            }
+            else if (choice.equalsIgnoreCase("list")) {
                 System.out.println(list);
+            }
+            else if (choice.equalsIgnoreCase("name")) {
+                Friend[] fa = list.toArray();
+                Arrays.sort(fa, (Friend a , Friend b) -> (a.getName().compareTo(b.getName())));
+                for(Friend f : fa){
+                    System.out.println(f);
+                }
+            }
+            else if (choice.equalsIgnoreCase("reverse")) {
+                Friend[] fa = list.toArray();
+                Arrays.sort(fa, (Friend a , Friend b) -> (b.getName().compareTo(a.getName())));
+                for(Friend f : fa){
+                    System.out.println(f);
+                }
+            }
+            else if (choice.equalsIgnoreCase("rating")) {
+                Friend[] fa = list.toArray();
+                Arrays.sort(fa, (Friend a , Friend b) -> (Integer.compare(b.getRating(), a.getRating())));
+                for(Friend f : fa){
+                    System.out.println(f);
+                }
             }
         }
     }
